3d926df7c2fd5ddebd082e100962d020b4fa65e5
Raphael Goujet <raphael.goujet@gmail.com>
2019-10-17 10:43:58 +0200

    E02 Enemy AI completed

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int waypointIndex = -1;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        getNextWaypoint();
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        Vector3 dir = target.position - this.transform.position;
+        this.transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (dir.magnitude <= .2f)
+        {
+            getNextWaypoint();
+        }
+    }
+
+    void getNextWaypoint()
+    {
+        if (waypointIndex >= Waypoints.waypoints.Length - 1)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            target = Waypoints.waypoints[++waypointIndex];
+        }
+    }
+}
