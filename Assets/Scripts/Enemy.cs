49afa880e678547ce6c4fb4b5cfc46ba2b2712ec
Raphael Goujet <raphael.goujet@gmail.com>
2019-10-29 17:05:04 +0100

    E23 Health Bars Completed

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,19 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     public const string enemyTag = "EnemyTag";
+
+    [Header("Movement")]
     public float startSpeed = 0f;
     [HideInInspector]
     public float speed = 0f;
     public float minimumDistance = 0f;
 
-    [SerializeField]
-    private ParticleSystem deathEffect = null;
-    [SerializeField]
-    private float health = 100f;
+    [Header("Worth")]
     [SerializeField]
     private int reward = 50;
+    
+    [Header("Health")]
+    [SerializeField]
+    private ParticleSystem deathEffect = null;
+    private float health = 0f;
+    [SerializeField]
+    private float startHealth = 0f;
+    [SerializeField]
+    private Image healthBar = null;
 
     /// <summary>
     /// Awake is called when the script instance is being loaded.
@@ -21,11 +30,13 @@ public class Enemy : MonoBehaviour
     void Awake()
     {
         speed = startSpeed;
+        health = startHealth;
     }
 
     public void takeDamage(float damage)
     {
         health -= damage;
+        healthBar.fillAmount = health/startHealth;
 
         if (health <= 0f)
         {
