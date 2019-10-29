974b42198fec4fb1268b130584a9409a28b4710d
Raphael Goujet <raphael.goujet@gmail.com>
2019-10-29 15:28:57 +0100

    E22 Main Menu Completed

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private string levelToLoad = "";
+
+    public void play()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void quit()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+}
