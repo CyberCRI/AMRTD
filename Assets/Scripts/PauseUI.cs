2cc489b64849351416bcb993676fab92a428c57a
Raphael Goujet <raphael.goujet@gmail.com>
2019-10-29 15:26:32 +0100

    E21 Pause Menu Completed

---
+++ Assets/Scripts/PauseUI.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseUI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject ui = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            toggle();
+        }
+    }
+
+    public void toggle()
+    {
+        ui.SetActive(!ui.activeSelf);
+
+        if (ui.activeSelf)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void retry()
+    {
+        toggle();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void menu()
+    {
+        Debug.Log("MENU");
+    }
+}
