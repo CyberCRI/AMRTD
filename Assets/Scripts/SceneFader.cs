f2f52070a2cd3a8a2edf934d2655adb234ae72bd
Raphael Goujet <raphael.goujet@gmail.com>
2019-10-29 17:31:10 +0100

    E24 Fading WIP - basic

---
+++ Assets/Scripts/SceneFader.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneFader : MonoBehaviour
+{
+    [SerializeField]
+    private Image image;
+    [SerializeField]
+    private float fadeSpeed = 1f;
+
+    void Start ()
+    {
+        StartCoroutine(fadeIn());
+    }
+
+    private IEnumerator fadeIn()
+    {
+        float t = 1f;
+
+        // animate over time
+        while (t > 0f)
+        {
+            t -= Time.deltaTime * fadeSpeed;
+            image.color = new Color(0f, 0f, 0f, t);
+            yield return 0;
+        }
+    }
+}
