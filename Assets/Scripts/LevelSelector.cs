d536bd28fbca8d4eaf1b347f7a34bc3680a4f424
Raphael Goujet <raphael.goujet@gmail.com>
2019-10-30 10:05:39 +0100

    E26 Level Select completed; fixed issue - mousewheel scroll & click and drag working only on level buttons, not on background - by adding transparent background to Content object

---
+++ Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class LevelSelector : MonoBehaviour
+{
+    [SerializeField]
+    private SceneFader sceneFader = null;
+
+    public void select(string levelName)
+    {
+        sceneFader.fadeTo(levelName);
+    }
+}
