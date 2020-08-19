5d26261d88c8056c2bcf6f72274ee0d3c139eeff
Raphael Goujet <raphael.goujet@gmail.com>
2020-08-19 19:48:28 +0200

    fixed issue #123 "Collapsible audio controls"; deleted deprecated scenes

---
+++ Assets/Scripts/UI/SoundToggleButtonUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundToggleButtonUI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject extensiveControls = null;
+    [SerializeField]
+    private Toggle soundButton = null;
+
+    void Start()
+    {
+        extensiveControls.SetActive(soundButton.isOn);
+    }
+
+    public void togglePress()
+    {
+        extensiveControls.SetActive(soundButton.isOn);
+    }
+
+    public void unfoldedPress()
+    {
+        soundButton.isOn = !soundButton.isOn;
+        extensiveControls.SetActive(soundButton.isOn);
+    }
+}
