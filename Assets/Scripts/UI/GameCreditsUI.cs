e2fed579a6eb041a2d5189f39904e96b52409500
Raphael Goujet <raphael.goujet@gmail.com>
2020-11-13 12:18:48 +0100

    game credits: navigation

--- Assets/Scripts/UI/GameCreditsUI.cs
+++ Assets/Scripts/UI/GameCreditsUI.cs
@@ -9,19 +9,56 @@ using UnityEngine.UI;
 public class GameCreditsUI : MonoBehaviour
 {
     [SerializeField]
+    private bool autoSequentialDisplay = false;
+    [SerializeField]
     private float displayDuration;
     [SerializeField]
-    private GameObject[] screenArray;
+    private GameObject[] screenArray = null;
+    private SCREEN currentScreen = SCREEN.MEMBERS;
 
     public const string sceneName = "GameCredits";
 
+    public enum SCREEN {
+        MEMBERS = 0,
+        RESOURCES1 = 1,
+        RESOURCES2 = 2
+    }
+
     void Start()
+    {
+        hideAll();
+        displayScreen(currentScreen);
+        if (autoSequentialDisplay)
+        {
+            StartCoroutine(animate());
+        }
+    }
+
+    private void hideAll()
     {
         for (int i = 0; i < screenArray.Length; i++)
         {
             screenArray[i].SetActive(false);
         }
-        StartCoroutine(animate());
+    }
+
+    public void displayScreen(SCREEN index)
+    {
+        hideAll();
+        Debug.Log((int)index);
+        screenArray[(int)index].SetActive(true);
+    }
+
+    public void nextScreen()
+    {
+        currentScreen = (SCREEN) ((((int) currentScreen) + 1) % 3);
+        displayScreen(currentScreen);
+    }
+
+    public void previousScreen()
+    {
+        currentScreen = (SCREEN) ((((int) currentScreen) + 2) % 3);
+        displayScreen(currentScreen);
     }
 
     private IEnumerator animate()
